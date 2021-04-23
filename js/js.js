e54e6f329fbd95ca9ecf7cd804156e65910a8d43
Marcel Sena Rios <carlosmarcel@brq.com>
2021-04-23 11:53:15 -0300

    Inserindo game loop no background

--- js/js.js
+++ js/js.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
-    /**
-     * Event Function
-     */
     $("#inicio").click(function(){
         $("#inicio").hide();
         $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
         $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
         $("#fundoGame").append("<div id='inimigo2'></div>");
         $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+                    
+        var jogo = {}
+        jogo.timer = setInterval(function() {
+            movefundo();
+        },30);        
     });
+
+    function movefundo() {
+        let esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position",esquerda-1);
+    } 
 
 });
 
