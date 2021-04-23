66026353b34d5b6d002cfd170d9b51d27a5fa16b
Marcel Sena Rios <carlosmarcel@brq.com>
2021-04-23 10:44:15 -0300

    Primeiros comandos com JS

---
+++ js/js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    /**
+     * Events
+     */
+    $("#inicio").click(function(e){
+        $("#inicio").hide();
+        $("#fundoGame").append("<div id='jogador'></div>");
+        $("#fundoGame").append("<div id='inimigo1'></div>");
+        $("#fundoGame").append("<div id='inimigo2'></div>");
+        $("#fundoGame").append("<div id='amigo'></div>");
+    });
+
+
+
+    /**
+     * Functions
+     */
+    function start() {
+
+    } 
+
+});
+
